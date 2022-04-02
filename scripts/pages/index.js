ad5dd3172516aa73d28cbe400d2a04236ec4f5bf
Deltyx <alexismorin.pro@gmail.com>
2022-04-02 19:26:34 +0200

    Get Ingredients/Appliances/Ustensils

--- scripts/pages/index.js
+++ scripts/pages/index.js
@@ -8,10 +8,10 @@ function getRecipesList() {
     const recipesList = [];
     recipes.forEach(recipe => {
         
-        let ingredientList = [];
+        let ingredientsList = [];
         let ustensilsList = [];
         recipe.ingredients.forEach(ingredient => {
-            ingredientList.push(new Ingredient(ingredient.ingredient, ingredient.quantity, ingredient.unit));
+            ingredientsList.push(new Ingredient(ingredient.ingredient, ingredient.quantity, ingredient.unit));
         });
 
         recipe.ustensils.forEach(ustensil => {
@@ -19,20 +19,75 @@ function getRecipesList() {
         })
 
         recipesList.push(new Recipe(
-            recipe.id, recipe.name, recipe.servings, ingredientList, recipe.time,
+            recipe.id, recipe.name, recipe.servings, ingredientsList, recipe.time,
             recipe.description, recipe.appliance, ustensilsList));
     });
     return recipesList;
 }
 
-let test = getRecipesList();
+function getIngredientsList() {
+    const ingredientsList = [];
+    recipes.forEach(recipe => {
+        recipe.ingredients.forEach(ingredient => {
+            ingredientsList.push(new Ingredient(ingredient.ingredient, ingredient.quantity, ingredient.unit));
+        })
+    })
+    /*
+    ingredientList = ingredientList.filter((value, index, self) => {
+        index === self.findIndex((t) => {
+            t.ingredient === value.ingredient && t.quantity === value.quantity && t.unit === value.unit
+        })
+    })
+    */
+    return ingredientsList;
+}
 
-const recipeSection = document.getElementById('recipe-section');
-console.log(recipeSection)
-recipeSection.innerHTML = '';
-console.log(recipeSection)
-let html5 = '';
-test.forEach(item => {
-    html5 += item.render();
-})
-recipeSection.innerHTML = html5;
+function getAppliancesList() {
+    const appliancesList = [];
+    recipes.forEach(recipe => {
+        appliancesList.push(recipe.appliance);
+    })
+    return appliancesList;
+}
+
+function getUstensilsList() {
+    const ustensilsList = [];
+    recipes.forEach(recipe => {
+        recipe.ustensils.forEach(ustensil => {
+            ustensilsList.push(ustensil);
+        })
+    })
+    return ustensilsList;
+}
+
+function removeDuplicates(data) {
+    let unique = data.reduce(function (a, b) {
+        if(a.indexOf(b) < 0) a.push(b);
+        return a;
+    }, []);
+ 
+    return unique;
+}
+
+function displayRecipesList(list) {
+    const recipeSection = document.getElementById('recipe-section');
+    let htmlToDisplay = '';
+    list.forEach(item => {
+        htmlToDisplay += item.render();
+    })
+    recipeSection.innerHTML = htmlToDisplay; 
+}
+
+function init() {
+    displayRecipesList(getRecipesList());
+}
+
+init();
+
+let test1 = getIngredientsList();
+let test2 = getUstensilsList();
+let test3 = getAppliancesList()
+
+console.log(removeDuplicates(test1));
+console.log(removeDuplicates(test2));
+console.log(removeDuplicates(test3));
